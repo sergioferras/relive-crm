6779318f66a738f81dcb5decaac2b339735363e5
jorgeserras <caveiroserras@gmail.com>
2021-03-04 18:47:33 +0000

    Progress Coordinates Address

--- src/views/submit/step2.jsx
+++ src/views/submit/step2.jsx
@@ -12,13 +12,20 @@ export default function Step2(props) {
     const formatTitle = title.replace(/ /g, "%20")
     const formatSe = se.replace(/ /g, "%20")
 
+    const formatLocation = location.replace(/ /g, "%20")
+    const formatLocation2 = formatLocation.replace(/,/g, "%2C")
+
+    const formatCoordinates = coordinates.replace(/,/g, "%2C")
+
+    
+
     return (
         <div style={{ display: step !== 2 ? 'none' : 'inherit' }}>
             <Typography variant="h4" component="h4">
                 Media
             </Typography>
             <Divider className="divider" orientation="horizontal" /> {/* New%20property%20from%20AppSheet%20awaiting%20update%20from%20API%20PRE-${ID} */}
-            <iframe src={`https://relive.pt/submit/?title=${formatTitle}_${ID}&note=Propriedade%20${formatTitle}%20submetida%20por%20${formatSe}%20com%20ID%20PRE-${ID}&location=${location}&coordinates=${coordinates}`} loading="lazy" style={{ width: '100%', borderWidth: 'inherit', height: '1000px' }} title="Submeter Propriedade Relive">
+            <iframe src={`https://relive.pt/submit/?title=${formatTitle}_${ID}&note=Propriedade%20${formatTitle}%20submetida%20por%20${formatSe}%20com%20ID%20PRE-${ID}&location=${formatLocation2}&coordinates=${formatCoordinates}`} loading="lazy" style={{ width: '100%', borderWidth: 'inherit', height: '1000px' }} title="Submeter Propriedade Relive">
             </iframe>
             <Grid container>
                 <Button variant="contained" color="secondary" onClick={() => changeStep(1)}>
